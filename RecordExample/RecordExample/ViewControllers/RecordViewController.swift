ffc10ac3c03cad9f23adce935cdc928fabd0954e
KTH-INHA-16 <>
2022-03-22 11:57:26 +0900

    add: record view controller

---
+++ RecordExample/RecordExample/ViewControllers/RecordViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RecordViewController.swift
+//  RecordExample
+//
+//  Created by 김태훈 on 2022/03/22.
+//
+
+import UIKit
+
+class RecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
