167c173944383c833286875adbfabeb15a0647bf
KTH-INHA-16 <>
2022-04-12 16:11:08 +0900

    add

--- RecordSample/RecordSample/Audio/AudioPlayer.swift
+++ RecordSample/RecordSample/Audio/AudioPlayer.swift
@@ -8,11 +8,46 @@
 import Foundation
 import Combine
 import AVFoundation
+import SwiftUI
 
 final class AudioPlayer: NSObject, ObservableObject {
     private(set) var audioPlayer: AVAudioPlayer?
+    @Published var progress: Double = 0.0
+    @Published var isPlaying: Bool = false
+    var duration: Double = 0.0
     
     override init() {
         super.init()
+    }
+    
+    func load(fileName: String) {
+        guard var directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first, let url = Bundle.main.url(forResource: "music", withExtension: "mp3") else {
+            return
+        }
+        directoryURL.appendPathComponent(fileName)
+        
+        do {
+            print(directoryURL)
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.prepareToPlay()
+            audioPlayer?.volume = 1.0
+            duration = audioPlayer?.duration ?? 0.0
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func play() {
+        
+        isPlaying.toggle()
+        
+        if isPlaying {
+            print("play")
+            audioPlayer?.play()
+        } else {
+            print("pause")
+            audioPlayer?.pause()
+        }
+        print(audioPlayer?.isPlaying)
     }
 }
--- RecordSample/RecordSample/Views/PlayView.swift
+++ RecordSample/RecordSample/Views/PlayView.swift
@@ -10,12 +10,36 @@ import SwiftUI
 struct PlayView: View {
     @State var num: Float = 0.0
     @ObservedObject var viewModel: PlayViewModel
+    @ObservedObject var player = AudioPlayer()
     
     var body: some View {
         VStack {
-            Slider(value: $num)
-                .padding(.horizontal, 50.0)
+            
+            Text("33")
+            
+            Slider(value: $player.progress, in: 0...1, onEditingChanged: { _ in
+                let targetTime = player.duration * player.progress
+                player.audioPlayer?.currentTime = targetTime
+            })
+            .padding(.all, 50.0)
+            
+            Button(action: {
+                player.play()
+            }, label: {
+                Image(systemName: player.isPlaying ? "play" : "pause")
+                    .frame(width: 50, height: 50, alignment: .center)
+                    .foregroundColor(.orange)
+                    .clipShape(Circle())
+                    .overlay {
+                        Circle().stroke(.orange, lineWidth: 2)
+                    }
+            })
         }
+        .onAppear {
+            player.load(fileName: viewModel.fileName)
+        }
+        .navigationTitle(viewModel.fileName)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
